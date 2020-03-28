4a163b2163c953f8e918c395d7a48cdb2086ca1e
MultipleIllusionsi <seregaserega06@gmail.com>
2020-03-28 23:25:29 +0500

    added helmet for SEO tags

--- src/client/pages/UsersListPage.js
+++ src/client/pages/UsersListPage.js
@@ -1,19 +1,30 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchUsers } from "../actions";
+import { Helmet } from "react-helmet";
 
 const UsersListPage = ({ users, fetchUsers }) => {
   useEffect(() => {
-    // if (!users.length) {
-    fetchUsers();
-    // }
+    if (!users.length) {
+      fetchUsers();
+    }
   }, []);
+
+  const head = () => {
+    return (
+      <Helmet>
+        <title>{`${users.length} Users loaded`}</title>
+        <meta property="og:title" content="Users App" />
+      </Helmet>
+    );
+  };
 
   const renderUsers = () =>
     users.map(user => <li key={user.id}>{user.name}</li>);
 
   return (
     <div>
+      {head()}
       List of users <ul>{renderUsers()}</ul>
     </div>
   );
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -6,6 +6,7 @@ import Routes from "../client/Routes";
 
 import { Provider } from "react-redux";
 import serialize from "serialize-javascript";
+import { Helmet } from "react-helmet";
 
 export default (req, store, context) => {
   const content = renderToString(
@@ -16,9 +17,13 @@ export default (req, store, context) => {
     </Provider>
   );
 
+  const helmet = Helmet.renderStatic();
+
   return `
     <html>
       <head>
+        ${helmet.title.toString()}
+        ${helmet.meta.toString()}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
       </head>
       <body>
